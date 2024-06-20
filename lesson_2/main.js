04e7a83fe6b55db400348bb1571616b0823ba74b
NguyenDev15 <nguyenn13122005@gmail.com>
2024-06-20 19:32:48 +0700

    learn data types

---
+++ lesson_2/main.js
@@ -0,0 +1,33 @@
+// Ép kiểu dữ liệu 
+// let numberA;
+// let numberB;
+
+// numberA = Number(prompt("xin mời nhập A"));
+// numberB = Number(prompt("xin mời nhập B"));
+
+// let kq = numberA + numberB;
+// console.log(`%s + %s = %s`,numberA,numberB,kq);
+// console.log(typeof kq); 
+
+// Toán tử 
+
+let a = 7;
+a += 3;
+console.log(`a = %s`,a);
+
+let b = 15;
+b -= 6
+console.log(`b = %s`,b);
+
+let c = 5;
+c *= 4;
+console.log(`c = %s`,c);
+
+let e = 5, f = 2;
+e -= f + 1;
+console.log(`e = %s`,e);
+
+
+
+
+
