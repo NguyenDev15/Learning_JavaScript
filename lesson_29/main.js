d1429e77c361fe4200b5f4de593d6d279f857bd6
NguyenDev15 <nguyenn13122005@gmail.com>
2024-07-26 22:08:32 +0700

    String Methods

---
+++ lesson_29/main.js
@@ -0,0 +1,22 @@
+// concat(), toUpperCase(), toLowerCase(), charAt();
+
+// concat() => nối chuỗi
+let ho = "Lionel";
+let ten = "Messi";
+let fullTen = ho.concat(" ", ten); // " " là dấu cách
+console.log(`Tên của bạn là ${fullTen}`);
+
+// toUpperCase -> in hoa
+
+let toUpper = ten.toUpperCase();
+console.log(toUpper);
+
+// toLowerCase -> in thường
+
+let toLower = ten.toLowerCase();
+console.log(toLower);
+
+// charAt() -> trả về giá trị index truyền vào
+
+let charAt = ten.charAt(4);
+console.log(charAt);
